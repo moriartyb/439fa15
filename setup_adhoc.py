920367f16fbf96215b512de8fa15ed97ced77810
bladechapman <bechapm2@illinois.edu>
2015-11-04 22:12:13 -0600

    more debugging

--- setup_adhoc.py
+++ setup_adhoc.py
@@ -13,9 +13,9 @@ def initialize_adhoc(**kwargs):
     os.system('iwconfig %s mode ad-hoc' % interface)
     os.system('iwconfig %s channel %s' % (interface, channel))
     os.system("iwconfig %s essid '%s'" % (interface, essid))
-    os.system('iwconfig %s key 1234567890' %interface)
+    os.system('iwconfig %s key 1234567890' % interface)
     os.system('ip link set %s up' % interface)
-    os.system('ip addr add %s dev %s', (ip, interface))
+    os.system('ip addr add %s dev %s' (ip, interface))
     os.system('iwconfig')
 
 if __name__ == '__main__':
