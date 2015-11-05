195ff7396b119c11ce8fbec4d24da17d68d07ebd
xColonelPanic <agadboi2@illinois.edu>
2015-11-04 21:44:49 -0600

    escape all the backslashes

--- test.py
+++ test.py
@@ -12,7 +12,7 @@ def post(val):
         print response.status, response.reason
 
 while True:
-	os.system("iwlist wlp1s0 scan | sed -ne 's#^[[:space:]]*\(Quality=\|Encryption key:\|ESSID:\)#\1#p' -e 's#^[[:space:]]*\(Mode:.*\)$#\1\n#p' | grep -B 2 hoc | grep -vE 'hoc|Encryption' > data.txt")
+	os.system("iwlist wlp1s0 scan | sed -ne \'s#^[[:space:]]*\\(Quality=\\|Encryption key:\\|ESSID:\\)#\\1#p\' -e \'s#^[[:space:]]*\\(Mode:.*\\)$#\\1\\n#p\' | grep -B 2 hoc | grep -vE \'hoc|Encryption\' > data.txt")
 	a = open("data.txt", "r")
         while a.readline() == '':
                 pass
