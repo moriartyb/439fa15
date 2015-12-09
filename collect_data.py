84a21ce83c82991aa9c97e1d639adcf39aa5356b
bladechapman <bechapm2@illinois.edu>
2015-12-08 19:53:33 -0600

    packages

--- collect_data.py
+++ collect_data.py
@@ -1,3 +1,4 @@
+import os
 
 cmd = "iwlist wlp1s0 scan | sed -ne \'s#^[[:space:]]*\\(Quality=\\|Encryption key:\\|ESSID:\\)#\\1#p\' -e \'s#^[[:space:]]*\\(Mode:.*\\)$#\\1\\n#p\' | grep -B 2 hoc | grep -vE \'hoc|Encryption\' > last_datum.txt"
 
