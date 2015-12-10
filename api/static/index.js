632eac73d8485b4e893ab8c2a9c702be65859dda
bladechapman <bechapm2@illinois.edu>
2015-12-09 23:25:52 -0600

    distance chart

--- api/static/index.js
+++ api/static/index.js
@@ -1,11 +1,13 @@
 $(document).ready(function() {
-    var chart;
+    var rssi_chart;
+    var distance_chart;
+
     var data_dict = {}
     var socket = io();
 
-    (function init_chart() {
-        chart = c3.generate({
-            bindto: '#chart',
+    (function init_rssi_chart() {
+        rssi_chart = c3.generate({
+            bindto: '#rssi_chart',
             data: {
                 columns: []
             },
@@ -29,6 +31,32 @@ $(document).ready(function() {
         });
     })();
 
+    (function init_distance_chart() {
+        distance_chart = c3.generate({
+            bindto: '#distance_chart',
+            data: {
+                columns: []
+            },
+            axis: {
+                y: {
+                    label: {
+                        text: 'Estimated distance (m)',
+                        position: 'middle-left'
+                    }
+                },
+                x: {
+                    label: 'Timestamp',
+                    tick: {
+                        format: function(x) {
+                            return Math.floor(x * 100) / 100;
+                        },
+                        count: 10
+                    }
+                }
+            }
+        })
+    })();
+
     socket.on('new_data', function(data) {
         update_data(data);
     });
@@ -43,8 +71,29 @@ $(document).ready(function() {
             data_dict[mac_addr] = [mac_addr, signal_strength];
         }
 
-        var column_data = flatten_points_dict(data_dict);
-        chart.load({ columns: column_data });
+        var column_rssi_data = flatten_points_dict(data_dict);
+        rssi_chart.load({ columns: column_rssi_data });
+
+        var distance_dict = generate_range_data(data_dict);
+        var column_distance_data = flatten_points_dict(distance_dict);
+        distance_chart.load({columns: column_distance_data});
+    }
+
+    function generate_range_data(rssi_dict) {
+        var ret = {}
+        for (var key in rssi_dict) {
+            var rssi_data = rssi_dict[key];
+            var mac_addr = rssi_data[0];
+            var rssi_vals = rssi_data.splice(1);
+
+            ret[mac_addr] = [mac_addr];
+
+            for (var i in rssi_vals.length) {
+                ret[mac_addr].push(compute_range(rssi_vals[i]));
+            }
+        }
+
+        return ret;
     }
 
     function flatten_points_dict(dict) {
@@ -54,5 +103,12 @@ $(document).ready(function() {
         }
 
         return ret;
+    }
+
+    function compute_range(rssi_value) {
+        var div = -((rssi - A) / (10 * n));
+        var d = Math.pow(10, div);
+
+        return d;
     }
 });
